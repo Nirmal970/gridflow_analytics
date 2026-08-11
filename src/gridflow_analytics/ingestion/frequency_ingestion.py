116f626bae7f3bf4fccab99de9a3fe449b64a90d
Nirmal970 <nirmalkumar9701@gmail.com>
2026-08-11 16:29:08 +0530

    fuelmix and frequency ingestion

---
+++ src/gridflow_analytics/ingestion/frequency_ingestion.py
@@ -0,0 +1,128 @@
+import json
+import sys
+
+from datetime import datetime,timezone
+
+from pyspark.sql import Row
+from pyspark.sql.functions import col
+from pyspark.dbutils import DBUtils
+
+from gridflow_analytics.common.spark_session import get_spark_session
+from gridflow_analytics.common.adls_auth import configure_adls
+from gridflow_analytics.common.energymap_api import get
+from gridflow_analytics.common.logger import logger
+
+from gridflow_analytics.config.config import (BRONZE_CONTAINER,STORAGE_ACCOUNT,ENERGYMAP_FREQUENCY_URL)
+
+
+def fetch_frequency_data(dbutils,from_timestamp: str,to_timestamp: str) -> dict:
+
+    try:
+
+        logger.info("Fetching grid frequency data from EnergyMap.")
+
+        from_time = datetime.fromisoformat(from_timestamp.replace("Z","+00:00"))
+        to_time = datetime.fromisoformat(to_timestamp.replace("Z","+00:00"))
+
+        hours = int((to_time - from_time).total_seconds() / 3600)
+
+        if hours <= 0:
+            raise ValueError("to_timestamp must be greater than from_timestamp.")
+
+        params = {"hours": hours}
+
+        data = get(dbutils,ENERGYMAP_FREQUENCY_URL,params)
+
+        logger.info("Grid frequency data fetched successfully from EnergyMap.")
+
+        return data
+
+    except Exception:
+
+        logger.exception("Failed while fetching grid frequency data from EnergyMap.")
+
+        raise
+
+
+def write_bronze(spark,dbutils,data: dict,from_timestamp: str,to_timestamp: str):
+
+    try:
+
+        bronze_path = f"abfss://{BRONZE_CONTAINER}@{STORAGE_ACCOUNT}.dfs.core.windows.net/raw/electricity/frequency"
+
+        logger.info(f"Checking existing Frequency Bronze data: {bronze_path}")
+
+        try:
+
+            existing_df = spark.read.json(bronze_path)
+
+            existing_count = existing_df.filter((col("source") == "energymap") & (col("dataset") == "grid_frequency") & (col("from_timestamp") == from_timestamp) & (col("to_timestamp") == to_timestamp)).count()
+
+            if existing_count > 0:
+
+                logger.info(f"Frequency Bronze data already exists for {from_timestamp} to {to_timestamp}. Skipping ingestion.")
+
+                return
+
+        except Exception:
+
+            logger.info("Bronze path does not exist. Initializing new Bronze dataset.")
+
+        raw_json = json.dumps(data)
+
+        bronze_df = spark.createDataFrame([
+            Row(
+                source="energymap",
+                dataset="grid_frequency",
+                from_timestamp=from_timestamp,
+                to_timestamp=to_timestamp,
+                ingestion_timestamp=datetime.now(timezone.utc),
+                raw_response=raw_json
+            )
+        ])
+
+        logger.info(f"Writing Frequency Bronze Data: {bronze_path}")
+
+        bronze_df.write.mode("append").json(bronze_path)
+
+        logger.info("Frequency Bronze layer written successfully.")
+
+    except Exception:
+
+        logger.exception("Failed while writing Frequency Bronze layer.")
+
+        raise
+
+
+def main():
+
+    from_timestamp = sys.argv[1]
+    to_timestamp = sys.argv[2]
+
+    spark = get_spark_session()
+
+    try:
+
+        dbutils = DBUtils(spark)
+
+        configure_adls(spark,dbutils)
+
+        logger.info(f"Processing frequency data from {from_timestamp} to {to_timestamp}")
+
+        logger.info("Starting Frequency Bronze ingestion.")
+
+        data = fetch_frequency_data(dbutils=dbutils,from_timestamp=from_timestamp,to_timestamp=to_timestamp)
+
+        write_bronze(spark=spark,dbutils=dbutils,data=data,from_timestamp=from_timestamp,to_timestamp=to_timestamp)
+
+        logger.info("Frequency Bronze ingestion completed successfully.")
+
+    except Exception as exc:
+
+        logger.exception(f"Frequency Bronze ingestion failed: {exc}")
+
+        raise
+
+
+if __name__ == "__main__":
+    main()
