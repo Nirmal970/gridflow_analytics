7ac30d134cf94fa1315515f6cf9dace3d5313a5e
Nirmal970 <nirmalkumar9701@gmail.com>
2026-08-16 11:54:01 +0530

    l

--- src/gridflow_analytics/config/config.py
+++ src/gridflow_analytics/config/config.py
@@ -27,6 +27,6 @@ ENERGYMAP_API_KEY_SECRET = "energymap-api-key"
 
 ENERGYMAP_FETCH_HOURS = 1440
 
-WEATHER_STATES = ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh",
+FETCH_STATES = ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh",
 "Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal",
-"Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli and Daman and Diu","Delhi","Jammu and Kashmir","Ladakh","Lakshadweep","Puducherry"]
+"Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli and Daman and Diu","Delhi","Jammu and Kashmir","Ladakh","Lakshadweep","Puducherry"] 
