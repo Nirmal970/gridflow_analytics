0d4d1eba1154f2e1a47c8ebcf4e8b7cb082f813f
Nirmal970 <nirmalkumar9701@gmail.com>
2026-08-16 20:18:30 +0530

    fix

--- src/gridflow_analytics/processing/weather_silver.py
+++ src/gridflow_analytics/processing/weather_silver.py
@@ -29,8 +29,6 @@ def main():
 
         df = df.select(col("state").cast("string").alias("state"),explode_outer(col("points")).alias("point"))
 
-        df = df.select(col("state_data.state").cast("string").alias("state"),explode_outer(col("state_data.points")).alias("point"))
-
         df = df.select(col("state"),to_timestamp(col("point.ts_utc")).alias("timestamp"),col("point.temperature_2m").cast("double").alias("temperature"),col("point.apparent_temperature").cast("double").alias("apparent_temperature"),col("point.relative_humidity_2m").cast("double").alias("relative_humidity"),col("point.dewpoint_2m").cast("double").alias("dewpoint"),col("point.wind_speed_10m").cast("double").alias("wind_speed"),col("point.wind_direction_10m").cast("double").alias("wind_direction"),col("point.surface_pressure").cast("double").alias("surface_pressure"),col("point.cloud_cover").cast("double").alias("cloud_cover"),col("point.precipitation").cast("double").alias("precipitation"),col("point.ghi_w_m2").cast("double").alias("ghi"),col("point.direct_radiation").cast("double").alias("direct_radiation"),col("point.dni_w_m2").cast("double").alias("dni"),col("point.dhi_w_m2").cast("double").alias("dhi"))
 
         df = df.filter(col("timestamp").isNotNull() & col("state").isNotNull())
