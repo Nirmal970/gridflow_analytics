ab1e139f885699a8fe6e9ae12b0e0e8f813888f2
Nirmal970 <nirmalkumar9701@gmail.com>
2026-08-16 11:22:49 +0530

    api changes

--- src/gridflow_analytics/config/config.py
+++ src/gridflow_analytics/config/config.py
@@ -22,3 +22,11 @@ ENERGYMAP_NATIONAL_DEMAND_HOURS = 24
 ENERGYMAP_REQUEST_TIMEOUT_SECONDS = 60
 ENERGYMAP_SECRET_SCOPE = "gridflow-dev-adls"
 ENERGYMAP_API_KEY_SECRET = "energymap-api-key" 
+
+
+
+ENERGYMAP_FETCH_HOURS = 1440
+
+WEATHER_STATES = ["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh",
+"Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal",
+"Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli and Daman and Diu","Delhi","Jammu and Kashmir","Ladakh","Lakshadweep","Puducherry"]
