dac31bee29a45cf9519a818e039cd835216de194
Nirmal970 <nirmalkumar9701@gmail.com>
2026-08-17 20:08:04 +0530

    fetch hrs for fuelmix

--- src/gridflow_analytics/ingestion/national_fuelmix_ingestion.py
+++ src/gridflow_analytics/ingestion/national_fuelmix_ingestion.py
@@ -11,7 +11,7 @@ from gridflow_analytics.common.adls_auth import configure_adls
 from gridflow_analytics.common.energymap_api import get
 from gridflow_analytics.common.logger import logger
 
-from gridflow_analytics.config.config import (BRONZE_CONTAINER,STORAGE_ACCOUNT,ENERGYMAP_NATIONAL_FUELMIX_URL)
+from gridflow_analytics.config.config import (BRONZE_CONTAINER,STORAGE_ACCOUNT,ENERGYMAP_NATIONAL_FUELMIX_URL,ENERGYMAP_FETCH_HOURS)
 
 
 def fetch_fuelmix_data(dbutils,hours: int) -> dict:
@@ -88,10 +88,9 @@ def write_bronze(spark,dbutils,data: dict,from_timestamp: str,to_timestamp: str,
 
 def main():
 
-    hours = 48
 
     to_time = datetime.now(timezone.utc)
-    from_time = to_time - timedelta(hours=hours)
+    from_time = to_time - timedelta(hours=ENERGYMAP_FETCH_HOURS)
 
     from_timestamp = from_time.isoformat().replace("+00:00","Z")
     to_timestamp = to_time.isoformat().replace("+00:00","Z")
@@ -108,9 +107,9 @@ def main():
 
         logger.info("Starting National Fuel Mix Bronze ingestion.")
 
-        data = fetch_fuelmix_data(dbutils=dbutils,hours=hours)
+        data = fetch_fuelmix_data(dbutils=dbutils,hours=ENERGYMAP_FETCH_HOURS)
 
-        write_bronze(spark=spark,dbutils=dbutils,data=data,from_timestamp=from_timestamp,to_timestamp=to_timestamp,hours=hours)
+        write_bronze(spark=spark,dbutils=dbutils,data=data,from_timestamp=from_timestamp,to_timestamp=to_timestamp,hours=ENERGYMAP_FETCH_HOURS)
 
         logger.info("National Fuel Mix Bronze ingestion completed successfully.")
 
